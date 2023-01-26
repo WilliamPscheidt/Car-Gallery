963e7c862fe0ae217d0d27da69c990bc39b8cc86
William Pscheidt <williampscheidt@hotmail.com>
2023-01-25 23:07:08 -0300

    Cluster to use all server CPUs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,29 @@
 import {HTTPServer} from "./adapters/httpserver.adapter";
 import DatabaseServer from "./adapters/DatabaseServer.adapter"
 
+import cluster from "cluster";
+import os from 'os';
+import process from "process";
+
 new DatabaseServer().connect()
-new HTTPServer().server.listen(3000, () => {
-    console.log("Server online! =)");
-});
+
+const cpusDisponible = os.cpus().length;
+
+if (cluster.isPrimary){
+    console.log(`Primary ${process.pid} is running`);
+
+    for (let processFork = 0; processFork < cpusDisponible; processFork++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died`)
+    });
+} else {
+
+    new HTTPServer().server.listen(3000, () => {
+        console.log("Server online! =)");
+    });
+
+    console.log(`Worker ${process.pid} started`)
+}

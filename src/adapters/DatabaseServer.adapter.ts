2a7ffa99a717332c32cd0f3e0a2ab9a12757ceb9
William Pscheidt <williampscheidt@hotmail.com>
2023-01-25 23:17:41 -0300

    Pool configurations in database connection

--- src/adapters/DatabaseServer.adapter.ts
+++ src/adapters/DatabaseServer.adapter.ts
@@ -6,8 +6,17 @@ class DatabaseServer {
     private uri = configurations.database.connection_string;
 
     public async connect(): Promise<void> {
+
+        const options = {
+            autoIndex: false,
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            family: 4
+          };
+
         try {
-            await mongoose.connect(this.uri)
+            await mongoose.connect(this.uri, options)
             console.log("MongoDB connected")
         } catch (error) {
             console.log("MongoDB error", error)
